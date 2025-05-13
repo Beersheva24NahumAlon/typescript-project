f1f3bc414ab6f9b353f99b9489dd0a0a17d0cef9
Nahum Alon <nochum.shepsl@gmail.com>
2025-05-13 23:08:40 +0300

    function update

--- src/main.ts
+++ src/main.ts
@@ -9,30 +9,16 @@ type Person = {
   }
 }
 
-type ReadOnlyPerson = {
-  readonly id: number,
-  readonly name: string,
-  readonly age: number,
-  readonly gender?: string,
-  readonly address?: {
-    city: string,
-    street: string
-  }
+function update<T, K extends keyof T>(object: T, field: K, value: T[K]): void {
+    object[field] = value;
 }
 
-type ReadOnlyPerson2 = {
-  readonly [K in keyof Person]: Person[K]
-}
-
-type ReadOnlyType<T> = {
-  readonly [K in keyof T]: T[K]
-}
-
-const person1: Person = { id: 123, name: "Vasya", age: 25 };
-person1.name = "Petya";
-const person2: ReadOnlyPerson = { id: 123, name: "Vasya", age: 25 };
-// person2.name = "Petya"; //not work
-const person3: ReadOnlyPerson2 = { id: 123, name: "Vasya", age: 25 };
-// person3.name = "Petya"; //not work
-const person4: ReadOnlyType<Person> = { id: 123, name: "Vasya", age: 25 };
-
+const person: Person = {id: 123, name: "Vasya", age: 25};
+console.log("before updating:");
+console.log(person);
+console.log("updating age to 26:");
+update(person, "age", 26);
+console.log("updating name to 'Petya':");
+update(person, "name", "Petya");
+console.log("after updating:");
+console.log(person);
