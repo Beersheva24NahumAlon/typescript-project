449f23a47746f87d302be351835fe14d66abe4a7
Nahum Alon <nochum.shepsl@gmail.com>
2025-05-14 18:20:01 +0300

    added store implementation based on map

--- src/StoreMapImpl.ts
+++ src/StoreMapImpl.ts
@@ -2,37 +2,42 @@ import Item from "./Item";
 import Predicate from "./Predicate";
 import Store from "./Store";
 
-export default class StoreArrayImpl implements Store {
+export default class StoreMapImpl implements Store {
 
-    constructor(private _items: Item[]) {}
+    constructor(private _items: Map<number, Item>) {}
 
     addItem(item: Item): void {
         const id = item.id;
         if (this.getItem(id) != null) {
             throw Error(`item with id ${id} already exists in the store`);
         }
-        this._items.push(item);
+        this._items.set(id, item);
     }
+
     getItem(id: number): null | Item {
-        const res = this._items.find((item) => item.id == id);
+        const res = this._items.get(id);
         return res == undefined ? null : res;
     }
+
     removeItem(id: number): void {
         if (this.getItem(id) == null) {
             throw Error(`item with id ${id} doesn't exist in the store`);
         }
-        const index = this._items.findIndex((item) => item.id == id);
-        this._items.splice(index, 1);
+        this._items.delete(id);
     }
+
     findByPredicate(predicate: Predicate): Item[] {
-        return this._items.filter(predicate);
+        const items = this._items.values();
+        return Array.from(items).filter(predicate);
     }
+
     removeByPredicate(predicate: Predicate): void {
         const removeItems = this.findByPredicate(predicate);
         removeItems.forEach(item => this.removeItem(item.id));
     }
+    
     getAllItems(): Item[] {
         return this.findByPredicate(_item => true);
     }
+    
 }
-
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,22 @@
 import Item from "./Item";
 import SimpleItem from "./SimpleItem";
 import StoreArrayImpl from "./StoreArrayImpl";
+import StoreMapImpl from "./StoreMapImpl";
 
-const items: Item[] = [
-  new SimpleItem(1, "item1", 200),
-  new SimpleItem(2, "item2", 250),
-  new SimpleItem(3, "item3", 300),
-];
-const store = new StoreArrayImpl(items);
+const item1 = new SimpleItem(1, "item1", 200);
+const item2 = new SimpleItem(2, "item2", 250);
+const item3 = new SimpleItem(3, "item3", 300);
+
+const arrayItems: Item[] = [item1, item2, item3];
+const mapItems = new Map<number, Item>([
+  [1, item1],
+  [2, item2],
+  [3, item3]
+]);
+
+//const store = new StoreArrayImpl(arrayItems);
+const store = new StoreMapImpl(mapItems);
+
 console.log("init items in store");
 console.log(store.getAllItems());
 console.log("adding new item with id 4");
@@ -24,6 +33,6 @@ console.log(store.getAllItems());
 // store.removeItem(3);
 console.log("getting items with price < 250");
 console.log(store.findByPredicate(item => item.price < 250));
-console.log("removibg items with price < 250");
+console.log("removing items with price < 250");
 console.log(store.removeByPredicate(item => item.price < 250));
 console.log(store.getAllItems());
